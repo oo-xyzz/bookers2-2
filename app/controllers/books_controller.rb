571dc262033a02cb9d7d2c4f03a6a30148b9bb84
EC2 Default User <ec2-user@ip-172-31-2-218.ap-northeast-1.compute.internal>
2024-05-14 11:20:36 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+  
+   private
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(current_user.id)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
+end
